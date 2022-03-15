273c282404d9833f88f3534f02be42ca79844b1d
omshah11 <61638057+omshah11@users.noreply.github.com>
2022-03-15 00:58:42 -0700

    Made changes to the header and navbar

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import {Button, dark} from "grommet"
+import {
+	FaInstagram,
+	FaLinkedin,
+	FaYoutube
+} from "react-icons/fa";
+import { FiMail } from "react-icons/fi";
+import {Button} from "grommet"
 import "./Navbar.css";
 const Navbar = () => {
     return(
         <section className='navbar'>
-            <a href="/" className="navbar-item">
-                <Button primary label="About" color={"#000000"} style={{ borderRadius: "2px" }} hoverIndicator />
-            </a>
-            <a href="/about" className="navbar-item">
-                <Button primary label="Academics" color={"#000000"} style={{ borderRadius: "2px" }} hoverIndicator />
-            </a>
-            <a href="/portfolio" className="navbar-item">
-                <Button primary label="Projects" color={"#000000"} style={{ borderRadius: "2px" }} hoverIndicator />
-            </a>
-            <a href="/shop" className="navbar-item">
-                <Button primary label="Contact" color={"#000000"} style={{ borderRadius: "2px" }} hoverIndicator />
-            </a>
+            <section className='navbar-content'>
+                <a href="/" className="navbar-item">
+                    <Button primary label="About" color={"#000000"} style={{ borderRadius: "8px", fontSize: "22px" }} hoverIndicator />
+                </a>
+                <a href="/about" className="navbar-item">
+                    <Button primary label="Academics" color={"#000000"} style={{ borderRadius: "8px", fontSize: "22px" }} hoverIndicator />
+                </a>
+                <a href="/portfolio" className="navbar-item">
+                    <Button primary label="Projects" color={"#000000"} style={{ borderRadius: "8px", fontSize: "22px" }} hoverIndicator />
+                </a>
+                <a href="/shop" className="navbar-item">
+                    <Button primary label="Contact" color={"#000000"} style={{ borderRadius: "8px", fontSize: "22px" }} hoverIndicator />
+                </a>
+            </section>
+            <section className='navbar-socials'>
+                <a className= "socials-item" href='mailto:om.shah@student.csulb.edu'><FiMail /></a>
+                <a className= "socials-item" href='linkedin.com/in/om-j-shah'><FaLinkedin /></a>
+                <a className= "socials-item" href='https://www.instagram.com/omshah11/'><FaInstagram /></a>
+                <a className= "socials-item" href="https://www.youtube.com/channel/UCz4h5LtIVN186q2iKlvEz0w"><FaYoutube /></a>
+            </section>
         </section>
     )
 }
